82c3bac49e6595dbaf9ad932b1c21af7b04e91e5
Mario Milošević <mario.milosevic@rbt.rs>
2025-09-26 20:00:21 +0200

    Add first style

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.view}>
       <Text>Edit app/index.tsx to edit this screen. Mario</Text>
+      <Link href="/login">Login page</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
